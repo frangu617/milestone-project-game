148c8e0dc16453abd1d024e83364f9b87880d124
frangu617 <142462584+frangu617@users.noreply.github.com>
2023-10-14 11:50:51 -0700

    score counter is working, have to bring in the sprite now

--- assets/script/game.js
+++ assets/script/game.js
@@ -2,7 +2,23 @@ let canvas = document.getElementById("gameCanvas");
 let context = canvas.getContext("2d");
 let gameUI = canvas.getContext("2d");
 
-gameUI.font = "10px Consolas";
+let score = 0;
+
+gameUI.font = "10px Arial";
 gameUI.fillText("Score: 0", 10, 10);
 
 // Create a square object
+
+function update() {
+
+}
+
+function updateScore() {
+    gameUI.clearRect(10, 0, canvas.width - 10, 10);
+    score++
+    // Set the font and draw the new score
+    gameUI.font = "10px Arial";
+    gameUI.fillText(`Score: ${score}`, 10, 10);
+}
+// Start the game loop
+setInterval(update, 100);
